c7a376af208e32945ea79a1a6ac56261fd00c826
Andrea Rinaldi <andrea.rinaldi@spot.it>
2014-08-06 14:18:48 +0200

    working!!

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var compression = require('compression');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -8,8 +10,13 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', './app/views');
 
+app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/assets'));
 require('./lib/routes')(app);
 
+app.use(compression());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 app.use(logfmt.requestLogger());
 
 var port = Number(process.env.PORT || 5000);
